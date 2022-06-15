eccab26840987b411dfee5b8c5927d443e49f431
admin <admin@sphinx.edu>
2022-06-15 15:35:00 +0200

    update databases

--- main/models.py
+++ main/models.py
@@ -1,13 +1,23 @@
 from ast import mod
 from distutils.command.upload import upload
 from email.mime import image
+import os
+from random import Random
 from unicodedata import name
 from django.db import models
 from location_field.models.plain import PlainLocationField
+import uuid
+from django.utils.crypto import get_random_string
 
 
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('uploads/', filename)
 # Create your models here.
 class store(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     name = models.CharField(max_length=30)
     location = PlainLocationField(based_fields=['city'], zoom=7)
     data_create=models.DateField()
@@ -18,27 +28,30 @@ class store(models.Model):
          return self.name
 ####################################################################
 class section(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     name = models.CharField(max_length=30)
     store_id=models.ForeignKey('store',on_delete=models.CASCADE)
-    image=models.ImageField(upload_to='images/section/%y/%m/%d')
+    image=models.ImageField(upload_to=get_file_path)
     
     def __str__(self) -> str:
         return self.name
 ####################################################################
 class category(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     name = models.CharField(max_length=30)
     section_id = models.ForeignKey('section',on_delete=models.CASCADE)
-    image=models.ImageField(upload_to='images/categor/%y/%m/%d')
+    image=models.ImageField(upload_to=get_file_path)
    
     
     def __str__(self) -> str:
          return self.name
 ####################################################################
 class product(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     name = models.CharField(max_length=30)
     category_id=models.ForeignKey('category',on_delete=models.CASCADE)
     company_id=models.ForeignKey('company',on_delete=models.CASCADE)
-    image=models.ImageField(upload_to='images/product/%y/%m/%d')
+    image=models.ImageField(upload_to=get_file_path)
     current_cartitem_id=models.ForeignKey('cartitem',on_delete=models.CASCADE)
     unit_name=models.CharField(max_length=50)
     selling_price=models.DecimalField(max_digits=20, decimal_places=10)
@@ -53,10 +66,11 @@ class product(models.Model):
          return self.name
 ################################################################
 class customer(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     name = models.CharField(max_length=30)
     store_id=models.ForeignKey('store',on_delete=models.CASCADE)
     phone=models.CharField(max_length=11)
-    image=models.ImageField(upload_to='images/customer/%y/%m/%d')
+    image=models.ImageField(upload_to=get_file_path)
     location=PlainLocationField(based_fields=['city'], zoom=7)
     market_name=models.CharField(max_length=50)
     address=models.CharField(max_length=50)
@@ -67,6 +81,7 @@ class customer(models.Model):
          return self.name
 ################################################################
 class  cart(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     store_id = models.ForeignKey('store',on_delete=models.CASCADE)
     manager_id=models.ForeignKey('customer',on_delete=models.CASCADE)
     date=models.DateField()
@@ -78,6 +93,7 @@ class  cart(models.Model):
 #################################################################
 
 class  cartitem(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     product_id = models.ForeignKey('product',on_delete=models.CASCADE)
     cart_id=models.ForeignKey('cart',on_delete=models.CASCADE)
     purchas_price=models.FloatField()
@@ -85,6 +101,7 @@ class  cartitem(models.Model):
     remaining=models.BigIntegerField()
 ##################################################################
 class  order(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     customer_id=models.ForeignKey('customer',on_delete=models.CASCADE)
     store_id=models.ForeignKey('store',on_delete=models.CASCADE)
     seller_id=models.ForeignKey('stuff',on_delete=models.CASCADE)
@@ -93,6 +110,7 @@ class  order(models.Model):
     sellin_price=models.DecimalField(max_digits=20, decimal_places=10)
 ################################################################
 class  orderitem(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     order_id=models.ForeignKey('order',on_delete=models.CASCADE)
     product_id=models.ForeignKey('product',on_delete=models.CASCADE)
     selling_price=models.BigIntegerField()
@@ -103,10 +121,12 @@ class  orderitem(models.Model):
     purchase_price=models.BigIntegerField()
 ###############################################
 class  ads(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     title=models.CharField(max_length=50)
     store_id=models.ForeignKey('store',on_delete=models.CASCADE)
 #################################################
 class month(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     store_id=models.ForeignKey('store',on_delete=models.CASCADE)
     date=models.DateField()
     sales_price=models.DecimalField(max_digits=20, decimal_places=10)
@@ -115,15 +135,17 @@ class month(models.Model):
     expensesDescription=models.CharField(max_length=50)
 #################################################################
 class company(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     name=models.CharField(max_length=50)
     store_id=models.ForeignKey('store',on_delete=models.CASCADE)
 ######################################################################
 class stuff(models.Model):
+    id = models.CharField(primary_key=True, default= uuid.uuid4, editable=False,max_length=40)
     list=( ('seller','seller'),('employe','employe'),('manager','manager'),('boss','boss') )
     name=models.CharField(max_length=50)
     store_id=models.ForeignKey('store',on_delete=models.CASCADE)
     phone=models.CharField(max_length=11)
-    image=models.ImageField(upload_to='images/stuff/%y/%m/%d')
+    image=models.ImageField(upload_to=get_file_path)
     role=models.CharField(max_length=50,choices=list)
 
 
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,14 @@
 
-from django.urls import path  ,include
+from django.urls import URLPattern, path  ,include
 from . import api
+from django.conf import settings
+
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('store/<int:id>', api.get_all_Section_in_store,name='get_all_Section_in_store'),
-    path('section/<int:id>', api.get_all_category_in_section,name='get_all_category_in_section'),
-    path('category/<int:id>', api.get_all_product_in_category,name='get_all_product_in_category'),
+    path('store/<str:id>', api.get_all_Section_in_store,name='get_all_Section_in_store'),
+    path('section/<str:id>', api.get_all_category_in_section,name='get_all_category_in_section'),
+    path('category/<str:id>', api.get_all_product_in_category,name='get_all_product_in_category'),
     
 ]
