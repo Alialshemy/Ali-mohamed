3264de3adb0668b7ead024ca93ab1db2f78d99a4
Alialshemy <ali.alshemy125@gmail.com>
2022-06-19 21:25:10 +0200

    add register

--- main/api.py
+++ main/api.py
@@ -3,9 +3,16 @@ from  rest_framework.response import Response
 from . import models
 from . import serializers
 from rest_framework.decorators import api_view
-from rest_framework.authentication import BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, mixins, viewsets
+from rest_framework import request, status, viewsets
+from django.contrib.auth.models import User
+
+from rest_framework.authentication import TokenAuthentication
+
+from rest_framework.permissions import  AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+
+from rest_framework.authtoken.models import Token
 #####################################################
 class get_all_Section_in_store(generics.ListAPIView):
     queryset = models.section.objects.all()
@@ -26,3 +33,22 @@ class get_all_product_in_category(generics.ListAPIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 ###################################################################
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+    # #authentication_classes = (TokenAuthentication, )
+    permission_classes = (AllowAny,)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        return Response({
+                'token': token.key, 
+                }, 
+            status=status.HTTP_201_CREATED)
+    
+    def list(self, request, *args, **kwargs):
+        response = {'message': 'Invild data'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
--- main/serializers.py
+++ main/serializers.py
@@ -14,3 +14,8 @@ class ProductSerializer(serializers.ModelSerializer):
         model = models.product
         fields = '__all__'
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
